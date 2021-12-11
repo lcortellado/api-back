9790cf70e72ff225f1e5fae6c5b3d551901b4df8
Luis Ortellado <luiscarlosortecabral@gmail.com>
2021-12-11 16:23:57 -0300

    Added route validation

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 const router = Router()
 
 import * as productsCtrl from '../controllers/products.controller'
+import { verifyToken } from '../middlewares'
 
-router.post('/', productsCtrl.createProduct)
+router.post('/', verifyToken, productsCtrl.createProduct)
 router.get('/', productsCtrl.getProducts)
 router.get('/:productId', productsCtrl.getProductById)
-router.put('/:productId', productsCtrl.updateProductById)
-router.delete('/:productId', productsCtrl.deleteProductById)
+router.put('/:productId', verifyToken, productsCtrl.updateProductById)
+router.delete('/:productId', verifyToken, productsCtrl.deleteProductById)
 
 export default router
